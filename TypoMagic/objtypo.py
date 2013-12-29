23630b3776a8e615b4fa9b1a99e1d0a06142df61
Ollie Whitehouse <Ollie.Whitehouse@nccgroup.com>
2013-12-29 13:02:47 +0000

    last fix ups for the new semi functional new AJAX UI (use /index.new.html)

---
+++ TypoMagic/objtypo.py
@@ -0,0 +1,37 @@
+class objtypo:
+    """typo object"""
+    
+    strDomain = ""
+    strHost = ""
+    bMX = False
+    bTypo = False
+    IPv4Address = []
+    IPV6Address = []
+    SafeBrowsing = ""
+    wwwv4 = []
+    wwwv6 = []
+    mv4 = []
+    mv6 = []
+    webmailv4 = []
+    webmailv6 = []
+    aMX = []
+
+    def __init__(self):
+        self.strDomain = ""
+        self.strHost = ""
+        self.bMX = False
+        self.bTypo = False
+        self.IPv4Address = []
+        self.IPV6Address = []
+        self.SafeBrowsing = ""
+        self.wwwv4 = []
+        self.wwwv6 = []
+        self.mv4 = []
+        self.mv6 = []
+        self.webmailv4 = []
+        self.webmailv6 = []
+        self.aMX = []
+
+    # http://stackoverflow.com/questions/5160077/encoding-nested-python-object-in-json
+    def reprJSON(self):
+        return dict(strDomain=self.strDomain, strHost=self.strHost, bMX=self.bMX, bTypo=self.bTypo, IPv4Addresses=self.IPv4Address, IPv6Addresses=self.IPV6Address, SafeBrowsing = self.SafeBrowsing, wwwv4 = self.wwwv4, wwwv6 = self.wwwv6, mv4 = self.mv4, mv6 = self.mv6, webmailv4 = self.webmailv4, webmailv6 = self.webmailv6, aMX = self.aMX ) 
