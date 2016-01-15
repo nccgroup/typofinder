8a1db10378baba882ee179b7fc8fa66d4c627101
Ollie.Whitehouse <ollie.whitehouse@nccgroup.com>
2016-01-15 11:01:01 +0000

    More robust error handling, verbose option and ctrl-c handler

--- typoclient/typoclient.py
+++ typoclient/typoclient.py
@@ -17,51 +17,64 @@ import requests
 import simplejson
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--domain', help='domain to analyze', required=False, type=str, default='nccgroup.com')
-    parser.add_argument('-s', '--server',   help='server to use', required=False, type=str, default='https://labs.nccgroup.trust')
-    args = parser.parse_args()
 
-    requests.packages.urllib3.disable_warnings()
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-d', '--domain', help='domain to analyze', required=False, type=str, default='nccgroup.com')
+        parser.add_argument('-s', '--server',   help='server to use', required=False, type=str, default='https://labs.nccgroup.trust')
+        parser.add_argument('-v', '--verbose',   help='verbose output', required=False, dest='verbose', action='store_true')
+        parser.set_defaults(verbose=False);
+        args = parser.parse_args()
+
+        requests.packages.urllib3.disable_warnings()
     
-    strURLTypos  = args.server + "/typofinder/typov2.ncc"
-    strURLEntity = args.server +  "/typofinder/entitylight.ncc"
+        strURLTypos  = args.server + "/typofinder/typov2.ncc"
+        strURLEntity = args.server +  "/typofinder/entitylight.ncc"
 
-    strHTTPHdrs      = {'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 'Accept': 'text/plain'}
-    strTypoRequest   = "host="+args.domain+"&typos=typos&typoamount=1&tld=tld&bitflip=bitflip&homoglyph=homoglyph&doppelganger=doppelganger&charsetamount=&alexafilter=neveralexa"
-    strEntityRequest = "host="
+        strHTTPHdrs      = {'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 'Accept': 'text/plain'}
+        strTypoRequest   = "host="+args.domain+"&typos=typos&typoamount=1&tld=tld&bitflip=bitflip&homoglyph=homoglyph&doppelganger=doppelganger&charsetamount=&alexafilter=neveralexa"
+        strEntityRequest = "host="
     
-    arrTypoResp = requests.post(strURLTypos, data=strTypoRequest, headers=strHTTPHdrs, verify=False)
+        arrTypoResp = requests.post(strURLTypos, data=strTypoRequest, headers=strHTTPHdrs, verify=False)
 
-    if arrTypoResp.text.startswith("[!]"):
-        print(arrTypoRest.text)
-        sys.exit(0)
+        if arrTypoResp.text.startswith("[!]"):
+            print(arrTypoRest.text)
+            sys.exit(0)
 
-    strTypoJSON = arrTypoResp.json()
+        strTypoJSON = arrTypoResp.json()
 
-    for strTDomain in strTypoJSON:
+        for strTDomain in strTypoJSON:
         
-        dataFoo = strEntityRequest+strTDomain
-        
-        try:
+            dataFoo = strEntityRequest+strTDomain
+           
             arrDomainResp = requests.post(strURLEntity, data=dataFoo, headers=strHTTPHdrs, verify=False)
-            #print(arrDomainResp.text)
 
             try:
                 strDEntityJSON = arrDomainResp.json()
             
-                print("[i] " + strDEntityJSON['strDomain'])
-                if strDEntityJSON['bError'] is False:
-                    for server in strDEntityJSON['nameservers']:
-                        print ("[i]    --- " + server);
+                if args.verbose is True:
+                    print("[i] " + strDEntityJSON['strDomain'])
+                    if strDEntityJSON['bError'] is False:
+                        for server in strDEntityJSON['nameservers']:
+                            print ("[i]    --- " + server);
+                    else:
+                            print ("[!]    --- " + strDEntityJSON['strError'])
                 else:
-                        print ("[!]    --- " + strDEntityJSON['strError'])
+                    if strDEntityJSON['bError'] is False:
+                        print("[i] " + strDEntityJSON['strDomain'] + " has nameservers")                           
 
             except simplejson.scanner.JSONDecodeError:
                 print("[!] JSON decode error")
 
-        except ConnectionAbortedError:
-            print("[!] Connected abort for " + print(strTDomain.encode('cp437', 'replace')))
+    except ConnectionAbortedError:
+        print("[!] Connection abort for " + print(strTDomain.encode('cp437', 'replace')))
 
-        except ConnectionError:
-            print("[!] Connected error for " + print(strTDomain.encode('cp437', 'replace')))
+    except ConnectionError:
+        print("[!] Connection error for " + print(strTDomain.encode('cp437', 'replace')))
+
+    except KeyboardInterrupt:
+        pass
+
+    except:
+        type, value, traceback = sys.exc_info()
+        print("[i] Other error - " + str(type) + " - " + str(value))
