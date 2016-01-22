f429b9a50408925b466c4d2f172d1fd97650a98e
Ollie.Whitehouse <ollie.whitehouse@nccgroup.com>
2016-01-22 11:27:10 +0000

    unit tests and exception handling.. still much to do

--- howoldisdomain/howoldisdomain.py
+++ howoldisdomain/howoldisdomain.py
@@ -7,54 +7,56 @@ import sys
 
 def howold(strDomain):
 
-    strWhois = whois(args.domain).split('\n')
+    strWhois = whois(strDomain).split('\n')
 
     if strWhois[0].startswith('Empty response from') or strWhois[0].startswith('Rate limited by '):
         return None
 
-    for strLine in strWhois:
-        if strLine.startswith('Creation Date: '):
-            strDate = strLine[15:]
-            dateThen = dateutil.parser.parse(strDate)
-            dateThen = dateThen.replace(tzinfo=None)
-            dateNow  = datetime.now()
-            dateDiff = dateNow - dateThen
-            return dateDiff.days
+    try:
+        for strLine in strWhois:
+            if strLine.startswith('Creation Date: '):
+                strDate = strLine[15:]
+                dateThen = dateutil.parser.parse(strDate)
+                dateThen = dateThen.replace(tzinfo=None)
+                dateNow  = datetime.now()
+                dateDiff = dateNow - dateThen
+                return dateDiff.days
 
-    # FR
-    for strLine in strWhois:
-        strLine = strLine.strip(' ')
-        if strLine.startswith('created:     '):
-            strDate = strLine[15:]
-            dateThen = dateutil.parser.parse(strDate)
-            dateThen = dateThen.replace(tzinfo=None)
-            dateNow  = datetime.now()
-            dateDiff = dateNow - dateThen
-            return dateDiff.days
+        # FR
+        for strLine in strWhois:
+            strLine = strLine.strip(' ')
+            if strLine.startswith('created:     '):
+                strDate = strLine[15:]
+                dateThen = dateutil.parser.parse(strDate)
+                dateThen = dateThen.replace(tzinfo=None)
+                dateNow  = datetime.now()
+                dateDiff = dateNow - dateThen
+                return dateDiff.days
             
-    # UK
-    for strLine in strWhois:
-        strLine = strLine.strip(' ')
-        if strLine.startswith('Registered on: '):
-            strDate = strLine[15:]
-            dateThen = dateutil.parser.parse(strDate)
-            dateThen = dateThen.replace(tzinfo=None)
-            dateNow  = datetime.now()
-            dateDiff = dateNow - dateThen
-            return dateDiff.days
+        # UK
+        for strLine in strWhois:
+            strLine = strLine.strip(' ')
+            if strLine.startswith('Registered on: '):
+                strDate = strLine[15:]
+                dateThen = dateutil.parser.parse(strDate)
+                dateThen = dateThen.replace(tzinfo=None)
+                dateNow  = datetime.now()
+                dateDiff = dateNow - dateThen
+                return dateDiff.days
                      
 
-    # CN
-    for strLine in strWhois:
-        strLine = strLine.strip(' ')
-        if strLine.startswith('Registration Time: '):
-            strDate = strLine[19:]
-            dateThen = dateutil.parser.parse(strDate)
-            dateThen = dateThen.replace(tzinfo=None)
-            dateNow  = datetime.now()
-            dateDiff = dateNow - dateThen
-            return dateDiff.days
-    
+        # CN
+        for strLine in strWhois:
+            strLine = strLine.strip(' ')
+            if strLine.startswith('Registration Time: '):
+                strDate = strLine[19:]
+                dateThen = dateutil.parser.parse(strDate)
+                dateThen = dateThen.replace(tzinfo=None)
+                dateNow  = datetime.now()
+                dateDiff = dateNow - dateThen
+                return dateDiff.days
+    except ValueError:
+        return None
 
     return None
     
@@ -63,8 +65,25 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--domain', help='domain to analyze', type=str)
+    parser.add_argument('-t', '--test', help='unit tests',  dest='tests', action='store_true')
+    parser.set_defaults(tests=False)
     args = parser.parse_args()
 
-    resAge = howold(args.domain)
-    if resAge:
-       print(str(resAge))
+    if args.tests:
+        arryDomains  = ['nccgroup.trust','nccgroup.com','facebook.cn','facebook.com','facebook.fr','facebook.de','facebook.co.uk','facebook.net','facebook.gr','facebook.nl','facebook.se','facebook.fi','facebook.ae','facebook.cm','facebook.co'
+                        ,'facebook.it','facebook.es','facebook.za','facebook.ca','facebook.pl','facebook.su','facebook.ru','facebook.tw','facebook.jp','facebook.au','facebook.nz','facebook.ar','facebook.mx','facebook.is','facebook.io']
+        for strDomain in arryDomains:
+            resAge = howold(strDomain)
+            print("[i] " + strDomain + " - ", end="")
+            if resAge:
+                print(str(resAge) + " - pass")
+            else:
+                print(" unknown - fail")
+    else:
+        if args.domain and len(args.Domain) > 0:
+            resAge = howold(args.domain)
+            if resAge:
+                print(str(resAge))
+                sys.exit(0)
+            else:
+                sys.exit(-1)
